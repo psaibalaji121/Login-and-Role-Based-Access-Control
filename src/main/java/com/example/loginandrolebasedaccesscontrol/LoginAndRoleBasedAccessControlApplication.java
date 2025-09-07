3c388e6471caa826555879b9c660af61b139949e
Sai Balaji Peruru <psaibalaji@opsecsecurity.com>
2025-09-07 09:16:06 +0530

    Created a new project to make security features enabled

---
+++ src/main/java/com/example/loginandrolebasedaccesscontrol/LoginAndRoleBasedAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginandrolebasedaccesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRoleBasedAccessControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginAndRoleBasedAccessControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loginandrolebasedaccesscontrol/LoginAndRoleBasedAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginandrolebasedaccesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRoleBasedAccessControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
